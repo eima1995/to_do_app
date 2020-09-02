ca7ef048559cc65481211091a7d57a03982179e9
eimaRR <eimantas.bendorius@royaltyrange.com>
2020-09-02 14:14:03 +0300

    add title

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -6,6 +6,33 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Constants.TASKS_SCREEN_BACKGROUND_COLOR,
+      floatingActionButton: FloatingActionButton(
+          backgroundColor:
+              Constants.TASKS_SCREEN_FLOATING_ACTION_BUTTON_BACKGROUND_COLOR,
+          child: Icon(Icons.add),
+          onPressed: null),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+            alignment: Alignment.center,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "To do list",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text("to do: 1 | done: 1",
+                    style: TextStyle(color: Colors.white, fontSize: 18)),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -2,4 +2,10 @@ library constants;
 
 import 'package:flutter/cupertino.dart';
 
+// Colors
 const Color TASKS_SCREEN_BACKGROUND_COLOR = Color(0xFF111328);
+const Color TASKS_SCREEN_FLOATING_ACTION_BUTTON_BACKGROUND_COLOR =
+    Color(0xFF111328);
+
+// Strings
+const String TASK_SCREEN_TITLE = "To do list";
