deee61a2b30de1877fed2f5ad1cd8560185e0377
eimaRR <eimantas.bendorius@royaltyrange.com>
2020-09-02 13:14:10 +0300

    add tasks screen

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/util/constants.dart' as Constants;
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Constants.TASKS_SCREEN_BACKGROUND_COLOR,
+    );
+  }
+}
---
+++ lib/util/constants.dart
@@ -0,0 +1,5 @@
+library constants;
+
+import 'package:flutter/cupertino.dart';
+
+const Color TASKS_SCREEN_BACKGROUND_COLOR = Color(0xFF111328);
